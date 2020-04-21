dec664237c2e8d2979500eb9c0eadf9abf71bc15
sander hagen <sanderhagen@outlook.com>
2020-04-21 14:19:18 +0200

    added try catch and some reporting

--- unpackZip.py
+++ unpackZip.py
@@ -12,18 +12,30 @@ timeStart = datetime.datetime.now().replace(microsecond=0)
 # Be aware that the request key returns an array, so accessing the id is done like this: request['request'][0]['id']
 with zipfile.ZipFile("logs.zip", "r") as f:
     for date in f.namelist():
+        # if the path is a directory open it
         if(os.path.isfile(date)):
-            file = open(date)
+            # read contents of the file
+            file = open(date, "r")
+            # try expect to catch any exceptions
             try:
+                # loop over each line
                 for line in file:
                     totalRequests += 1
+                    # parse the request
                     request = json.loads(file.readline())
             except:
+                # log amount of errors
                 amountOfErrors += 1
+                # if debug is True, the program stops and reports the number of the request where it failed
                 if(debug):
-                    print("exception occurred at "+str(i)+"th request")
+                    print("exception occurred at "+str(totalRequests)+"th request")
                     break;
+            file.close()
 timeEnd = datetime.datetime.now().replace(microsecond=0)
-print("Script finished reading"+ str(totalRequests) + " requests.")
+print("Script finished reading "+ str(totalRequests) + " requests.")
 print(str(amountOfErrors)+" errors occurred.")
 print("Took "+str(timeEnd - timeStart)+" seconds")
+
+            
+
+
