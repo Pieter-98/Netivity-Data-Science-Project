b8bc4b7fa93429a8897800489145626111b39f5e
sander hagen <sanderhagen@outlook.com>
2020-04-21 18:29:24 +0200

    fixed blob support

--- unpackZip.py
+++ unpackZip.py
@@ -3,37 +3,44 @@ import os
 import json
 import datetime
 import sys
+import base64
 # To use this script, make sure the logs.zip file is in the same directory as this script. Then execute `python unpackZip.py` to run.
 
-totalRequests = 0
 amountOfErrors = 0
-debug = False
+totalRequests = 0
+debug = True
 timeStart = datetime.datetime.now().replace(microsecond=0)
 sys.stdout.write("Running\n")
 sys.stdout.flush()
+    
+def parseRequestsFromFile(filepath):
+    global amountOfErrors, totalRequests
+    try:
+        data = f.read(blobfilepath).decode()
+        requests = []
+        request = ""
+        for i in range(len(data)):
+            if data[i] == '\n':
+                totalRequests += 1
+                jsonfile = json.loads(request)
+                requests.append(jsonfile)
+                request = ""
+                continue
+            request = request + data[i]
+        return requests
+    except:
+        amountOfErrors += 1
+        
 # request is a Python dictionary which contains 3 keys, request, internal and context which can be accessed like 'request['request']'
 # Be aware that the request key returns an array, so accessing the id is done like this: request['request'][0]['id']
-with zipfile.ZipFile("logs.zip", "r") as f:
-    for date in f.namelist():
-        # if the path is a directory open it
-        if(os.path.isfile(date)):
+with zipfile.ZipFile("logs.zip") as f:
+    for blobfilepath in f.namelist():
+        # if the extension of the file is .blob, read the file.
+        if(os.path.splitext(blobfilepath)[1] == '.blob'):
             # read contents of the file
-            file = open(date, "r")
-            # try expect to catch any exceptions
-            try:
-                # loop over each line
-                for line in file:
-                    totalRequests += 1
-                    # parse the request
-                    request = json.loads(file.readline())
-            except:
-                # log amount of errors
-                amountOfErrors += 1
-                # if debug is True, the program stops and reports the number of the request where it failed
-                if(debug):
-                    print("exception occurred at "+str(totalRequests)+"th request")
-                    break;
-            file.close()
+            requests = parseRequestsFromFile(blobfilepath)
+    
+# Calculate time spent and report some statistics
 timeEnd = datetime.datetime.now().replace(microsecond=0)
 print("Script finished reading "+ str(totalRequests) + " requests.")
 print(str(amountOfErrors)+" errors occurred.")
